be3179dd671b975e17dd671de6a63a824fbb41e9
kki7823 <kjd55399997@gmail.com>
2021-11-08 15:00:49 +0900

    Initial commit

---
+++ src/main/java/com/doldolseo/doldolseo_msa_review/DoldolseoMsaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.doldolseo.doldolseo_msa_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoldolseoMsaReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoldolseoMsaReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doldolseo/doldolseo_msa_review/DoldolseoMsaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doldolseo.doldolseo_msa_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoldolseoMsaReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
